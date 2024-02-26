acda70ccd018bb5618471e7935efac4892b0a110
alager <aaron.lager@gmail.com>
2024-02-26 01:08:56 +0000

    testing

--- src/lawnmower.cpp
+++ src/lawnmower.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <fstream>
 
+#include <pigpio.h>
+
 int main() {
   // Create a file stream object
   std::ofstream outfile("hello_world.txt");
 
   // Write the string "hello world" to the file
-  outfile << "hello world";
+  outfile << "hello world2";
 
   // Close the file stream object
   outfile.close();
