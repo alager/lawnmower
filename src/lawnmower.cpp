65654b406af62c08c5bb09808d2adfc52807b9c7
alager <aaron.lager@gmail.com>
2024-03-08 05:13:04 +0000

    adding header file

--- src/lawnmower.cpp
+++ src/lawnmower.cpp
@@ -1,17 +1,33 @@
-#include <iostream>
-#include <fstream>
+#include "lawnmower.h"
 
-#include <pigpio.h>
 
 int main() {
-  // Create a file stream object
-  std::ofstream outfile("hello_world.txt");
+	// Create a file stream object
+	std::ofstream outfile("hello_world.txt");
 
-  // Write the string "hello world" to the file
-  outfile << "hello world2";
+	// Write the string "hello world" to the file
+	outfile << "hello world4";
 
-  // Close the file stream object
-  outfile.close();
+	// Close the file stream object
+	outfile.close();
 
-  return 0;
+	cout << "pre init" << endl;
+	if (gpioInitialise()<0) 
+		return 1;
+
+	cout << "post init" << endl;
+	gpioSetMode(HALL, PI_INPUT);
+	gpioSetPullUpDown(HALL, PI_PUD_UP);
+
+	printf("GPIO14 is level %d\n", gpioRead(HALL));
+	gpioSetPullUpDown(HALL, PI_PUD_DOWN);
+	printf("GPIO14 is level %d\n", gpioRead(HALL));
+
+
+	//gpioSetAlertFunc(HALL, alert);
+
+	//sleep(secs);
+
+	gpioTerminate();
+	return 0;
 }
