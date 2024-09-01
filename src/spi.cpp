b163ec74e557e4e61645c9b3913df3cd54934bad
alager <aaron.lager@gmail.com>
2024-08-31 19:24:25 -0500

    removed unused code

--- src/spi.cpp
+++ src/spi.cpp
@@ -3,12 +3,6 @@
 
 int Spi::write( char *data, unsigned count )
 {
-	// // flip endian for SPI chip required order
-	// data = htonl( data << 8 );
-
-	// // copy to the class register array
-	// memcpy( reg_, &data, sizeof( reg_ ) );
-
 	// write the spi data
 	return spiWrite( ( unsigned int )handle_, data, count );
 }
